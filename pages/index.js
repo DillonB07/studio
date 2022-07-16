61faa8d7cec173cf89c1bd20e1832b9bf3c13a22
Dillon Barnes <dillonbarnes07@gmail.com>
2022-07-16 19:03:19 +0100

    Add project links
    Temporary update until I finally finish this.

--- pages/index.js
+++ pages/index.js
@@ -3,58 +3,55 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>DillonB07</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+          <a className="text-blue-600" href="/">
+            DillonB07's Studio. 
           </a>
         </h1>
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.js
-          </code>
+          Sorry! This studio is still a work in progress because I keep getting distracted and making other projects. <em>Maybe it'll never get made...</em><br/>Check out these instead:
         </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
-            href="https://nextjs.org/docs"
+            href="https://reddit.dillonb07.studio"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">Reddit Clone</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+              A Reddit Clone built with NextJS, Supabase and Stepzen
             </p>
           </a>
           <a
-            href="https://nextjs.org/learn"
+            href="https://google.dillonb07.studio"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
             <h3 className="text-2xl font-bold">Learn &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
+              A Google clone built with NextJS
+             </p>
           </a>
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://expo.dev/@DillonB07/deliveroo-clone"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
             <h3 className="text-2xl font-bold">Examples &rarr;</h3>
             <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
+              Deliveroo clone built with React Native. You'll need the Expo Go mobile app to try this app for yourself.
             </p>
           </a>
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://github.com/DillonB07"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
             <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
             <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              My GitHub Profile!
             </p>
           </a>
         </div>
@@ -62,12 +59,12 @@ export default function Home() {
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://dillonb07.is-a.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
+          Created by{' '}
+          <img src="https://dillonb07.is-a.dev/static/images/DillonB07.png" alt="DillonB07's logo" className="ml-2 h-4 rounded-full" />
         </a>
       </footer>
     </div>
